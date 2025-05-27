1a8e91931cd45a1c5b9487cefd997f471947b293
GamerBoi801 <geeseplayer@gmail.com>
2025-05-27 21:55:51 +0500

    sorting algorithms && time complexity

---
+++ sorting/counting_sort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void count_sort(int arr[], int len);
+int main() {
+    
+}
+
+void count_sort(int arr[], int len) {
+    // 1st find the max, min value
+    int maxima = max(arr);
+    int minima = min(arr);
+
+
+    //initializing the count arrays
+    
+}
