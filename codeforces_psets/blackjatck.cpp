ecb05cba5380221921104022b6535176034e80d9
GamerBoi801 <geeseplayer@gmail.com>
2025-07-11 22:42:51 +0500

    blakcjack pset

---
+++ codeforces_psets/blackjatck.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    // need to determine how many ways to get 2nd card so that the sym ot pts = n
+    // first card taken is the Queen of spades(10 pts)
+
+    int ans = 0;
+    int required_points = n - 10;
+
+    // we can rationalize the dataset to meet certain circumstances
+
+    
+    if (required_points >= 11) {
+        // we can use an ace card 
+        
+    }
+
+}
