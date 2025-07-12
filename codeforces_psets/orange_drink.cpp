0a25bcaa31f62e0983d9c0cce86439fe31f4c93d
GamerBoi801 <geeseplayer@gmail.com>
2025-07-12 09:52:53 +0500

    orange_juice pset

--- codeforces_psets/orange_drink.cpp
+++ codeforces_psets/orange_drink.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 int main() {
-    
+    int n;
+    cin >> n;
+
+    float total = 0.0f;
+
+
+    for(int i = 0; i < n; i++) {
+        int p = 0;
+        cin >> p; 
+
+        float temp = p / 100.0;
+        total  = total + temp;
+    }
+
+    cout << (total /  n) * 100  << endl;
+
     return 0;
 }
