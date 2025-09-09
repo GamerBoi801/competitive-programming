5f538f187130c06a2d40446543276f6e6d7c1782
GamerBoi801 <geeseplayer@gmail.com>
2025-09-09 14:47:21 +0500

    move_brackets.cpp

---
+++ move_brackets.cpp
@@ -0,0 +1,17 @@
+// https://codeforces.com/problemset/problem/1374/C
+#include <iostream>
+using namespace std;
+
+int main() {
+    int first[99];
+    int last[99];
+
+    int t = 0; //number of test cases
+    cin >> t;
+
+    for(int i = 0; i < t; i++) {
+        
+    }
+
+
+}
