1d5e5d57781b0d6c74de3d40f585728a7fdc76e7
GamerBoi801 <geeseplayer@gmail.com>
2025-11-11 11:45:30 +0500

    Miscallenous commits to codeforces

---
+++ codeforces_psets/board_moves.cpp
@@ -0,0 +1,24 @@
+// https://codeforces.com/problemset/problem/1353/C
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t = 0; //test cases
+    cin >> t;
+
+    //loop for test cases
+    for(int i = 0; i < t; i++) {
+        int n = 0;
+        cin >> n; // for each test case dimension changes
+
+        //starting point
+        int starting_cord = n / 2;
+        
+
+    }
+
+    
+
+    return 0;   
+}
