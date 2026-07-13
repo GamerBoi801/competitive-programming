ad81cf02a7310131955870098414e658b024fd43
GamerBoi801 <geeseplayer@gmail.com>
2026-07-13 10:09:00 +0500

    THe popcorn problem

--- 800/2240a.cpp
+++ 800/2240a.cpp
@@ -1,14 +1,25 @@
+// https://codeforces.com/problemset/problem/2240/A
+#include <bitset>
 #include <iostream>
+#include <cmath>
+#include <string>
 using namespace std;
 
 string binary(int n) {
     // converts a num to bin as a string
-    long long i = 1;
-    while (i > 0) {
-        
-    }
+    string bin = bitset<32>(n).to_string();
+    return bin;
 }
 
+int popcorn(string s) {
+    int a= 0;
+    for (auto str : s) {
+        if (s == "1") {
+            a++;
+        }
+    }
+    return a;
+}
 
 int main() {
     int t; cin >> t;
