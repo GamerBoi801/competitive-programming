309607e2c04ac7e59538ad3dff01b5e74018959f
GamerBoi801 <geeseplayer@gmail.com>
2026-08-15 16:27:43 +0500

    more psets

---
+++ CSES/introductory/knights.cpp
@@ -0,0 +1,12 @@
+// https://cses.fi/problemset/task/1072
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int k; cin>> k;
+
+    for(int _ = 0; _ < k; _++) {
+
+    }
+    return 0;
+}
