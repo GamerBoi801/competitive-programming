4b5b800e2daeacc403279f5e539dea1b5c676034
GamerBoi801 <geeseplayer@gmail.com>
2026-08-15 16:26:50 +0500

    1899a pset

--- 800/1899a.cpp
+++ 800/1899a.cpp
@@ -5,8 +5,16 @@ using namespace std;
 int main() {
     int t; cin >> t;
     while (t--) {
-        int n;cin >> t;
+        int n;cin >> n;
 
+        // vanya's turn 
+        if (n % 3 == 1 || n % 3 == 2) {
+            // vanya can -1 at this turn since  1 -1 = 0
+            // she can +1 the 2 + 1 = 3 to becoem divisible 
+            cout << "First\n";
+        } else if (n % 3 == 0) {
+            cout << "Second\n";
+        }
     }
     return 0;
 }
